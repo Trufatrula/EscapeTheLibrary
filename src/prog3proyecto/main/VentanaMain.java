b3a238f5ffe5abbfc1e509d3ef32085617c2bd14
Andoni Torres <andoni.torres@opendeusto.es>
2021-10-28 16:18:44 +0200

    Ventanas y grid

--- src/prog3proyecto/main/VentanaMain.java
+++ src/prog3proyecto/main/VentanaMain.java
@@ -1,12 +1,16 @@
 package prog3proyecto.main;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.DefaultListCellRenderer;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -32,15 +36,44 @@ public class VentanaMain extends JFrame {
 	
 	private JPanel crearPanelPrincipal() {
 		JPanel panel = new JPanel();
+		panel.setLayout(new BorderLayout());
+		
+		JPanel panelN = new JPanel();
+		//panelN.setBackground(Color.green);
+		JPanel panelW = new JPanel();
+		//panelW.setBackground(Color.red);
+		panelW.setLayout(new BorderLayout());
+		JPanel panelWS = new JPanel();
+		//panelWS.setBackground(Color.black);
+		JPanel panelC = new JPanel();
+		//panelC.setBackground(Color.blue);
+		JPanel panelS = new JPanel();
+		//panelS.setBackground(Color.yellow);
+		
+		String nombres[] = {"Kaladin", "Shallan", "Dalinar", "Adolin", "Sagaz", "Navani", "Szeth"};
+		
 		JButton botonJugar = new JButton("JUGAR");
 		JButton botonOpciones = new JButton("OPCIONES");
+		JButton botonCrearUsuario = new JButton("Nuevo");
+		JButton botonBorrarUsuario = new JButton("Borrar");
 		JLabel lnombre = new JLabel("Nombre de usuario: ");
 		JTextField tnombre = new JTextField(15);
+		JList<String> a = new JList<String>(nombres);
+		DefaultListCellRenderer renderer =  (DefaultListCellRenderer)a.getCellRenderer();  
+		renderer.setHorizontalAlignment(JLabel.CENTER);  
 		
-		panel.add(lnombre);
-		panel.add(tnombre);
-		panel.add(botonJugar);
-		panel.add(botonOpciones);;
+		panel.add(panelN, BorderLayout.NORTH);
+		panel.add(panelW, BorderLayout.WEST);
+		panel.add(panelC, BorderLayout.CENTER);
+		panel.add(panelS, BorderLayout.SOUTH);
+		panelW.add(panelWS, BorderLayout.SOUTH);
+		panelN.add(lnombre);
+		panelN.add(tnombre);
+		panelS.add(botonJugar);
+		panelS.add(botonOpciones);
+		panelW.add(a, BorderLayout.CENTER);
+		panelWS.add(botonCrearUsuario);
+		panelWS.add(botonBorrarUsuario);
 		
 		botonJugar.addActionListener(new ActionListener() {
 			@Override
@@ -53,6 +86,20 @@ public class VentanaMain extends JFrame {
 		});
 		
 		botonOpciones.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				startOpciones();
+			}
+		});
+		
+		botonCrearUsuario.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				startOpciones();
+			}
+		});
+		
+		botonBorrarUsuario.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				startOpciones();
