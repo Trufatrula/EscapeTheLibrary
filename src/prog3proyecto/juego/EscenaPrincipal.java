ec2e84b5e9e643ba690ad6502a844a15c66d7101
Lander Gallastegi <lander.gallastegi@opendeusto.es>
2021-12-26 14:26:52 +0100

    Test Interact

--- src/prog3proyecto/juego/EscenaPrincipal.java
+++ src/prog3proyecto/juego/EscenaPrincipal.java
@@ -1,11 +1,16 @@
 package prog3proyecto.juego;
 
 import org.joml.Vector3f;
+import org.joml.Vector4f;
 
+import com.lndf.glengine.gl.DefaultMaterial;
+import com.lndf.glengine.physics.PhysicalMaterial;
+import com.lndf.glengine.primitives.Cube;
 import com.lndf.glengine.scene.GameObject;
 import com.lndf.glengine.scene.Scene;
 import com.lndf.glengine.scene.components.Camera;
-import com.lndf.glengine.scene.components.lighting.PointLight;
+import com.lndf.glengine.scene.components.physics.BoxCollider;
+import com.lndf.glengine.scene.components.physics.DynamicRigidBody;
 
 import prog3proyecto.main.DatosJugador;
 
@@ -17,7 +22,10 @@ public class EscenaPrincipal extends Scene {
 	private DatosJugador datos;
 	
 	public EscenaPrincipal(DatosJugador datos) {
-		
+		DefaultMaterial mat = new DefaultMaterial(new Vector4f(1,0,0,1), new Vector4f(1,1,1,1), 1);
+		DynamicRigidBody rigid = new DynamicRigidBody();
+		BoxCollider col = new BoxCollider(new PhysicalMaterial(1,1,1));
+		Cube c = new Cube(mat);
 		this.subscribeToUpdates();
 		this.addUpdateRunnable(new ActualizarEscena(this));
 		this.datos = datos;
@@ -28,6 +36,11 @@ public class EscenaPrincipal extends Scene {
 		this.addObject(this.jugador);
 		this.jugador.getTransform().setPosition(new Vector3f(0, 1.5f, 0));
 		this.setAmbientLight(0.1f);
+		c.getTransform().setPosition(new Vector3f(1,1,1));
+		c.getTransform().setScale(new Vector3f(0.1f,0.1f,0.1f));
+		c.addComponent(col);
+		c.addComponent(rigid);
+		this.addObject(c);
 	}
 	
 	public Camera getCamara() {
