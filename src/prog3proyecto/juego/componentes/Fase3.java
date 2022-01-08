2efc516dc880d42f3cdd8d112938afd44fadd2bb
Lander Gallastegi <landergallastegi@gmail.com>
2022-01-08 20:23:55 +0100

    Restablecer luz en fase 3

--- src/prog3proyecto/juego/componentes/Fase3.java
+++ src/prog3proyecto/juego/componentes/Fase3.java
@@ -10,6 +10,9 @@ public class Fase3 extends Component {
 	public void addToScene() {
 		EscenaPrincipal pepe = (EscenaPrincipal) this.getScene();
 		pepe.getDatos().setFase(3);
+		pepe.setAmbientLight(0.004f);
+		pepe.getJugador().getLuz().setRadius(10);
+		pepe.getJugador().getLuz().setStrength(0.5f);
 		pepe.getTerreno().movilizarPuerta();
 	}
 }
