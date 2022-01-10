3f65a9254d906d08d4fced6e04db80fe233ada3e
Andoni Torres <andoni.torres@opendeusto.es>
2022-01-10 21:43:12 +0100

    Fase3 Comenzada

---
+++ src/prog3proyecto/juego/PuzzleVasos.java
@@ -0,0 +1,13 @@
+package prog3proyecto.juego;
+
+import com.lndf.glengine.scene.GameObject;
+
+public class PuzzleVasos {
+	
+	private GameObject[] vasos = new GameObject[3];
+	
+	
+	public PuzzleVasos() {
+		
+	}
+}
